090040d0ad731ac7f758e405bb13b835927597ee
emiliano@asus <emromano@fi.uba.ar>
2026-06-07 13:44:55 -0300

    schemas y seeds

---
+++ backend de prueba (pokeAPI)/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO tipos (nombre) VALUES
+('Fuego'),
+('Agua'),
+('Planta');
+
+INSERT INTO pokemones (nombre, evolucion, tipo, rareza) VALUES
+('Charmander', 1, 1, 2),
+('Squirtle', 1, 2, 2),
+('Bulbasaur', 1, 3, 2),
+('Charmeleon', 2, 1, 3);
